204b60e6cce54e11ed72a71bc9b60888ff2b3a6b
priyanshukumar72 <priyanshu727989@gmail.com>
2025-05-03 13:52:15 +0530

    edit

--- model/connection.js
+++ model/connection.js
@@ -1,7 +1,6 @@
 const mongoose=require('mongoose')
 const {Schema}=mongoose.Schema;
 const dotenv=require('dotenv').config();
-//  const url='=mongodb+srv://sachin:8757887103@shopdata.shpwbu2.mongodb.net/';
  const url=process.env.ATLASDB_URL;
 main().then(()=>{
     console.log("db connected !");
